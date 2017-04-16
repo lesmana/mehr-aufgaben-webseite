82a2caddc4e107063f365186c32f6f869b39aa2d
Lesmana Zimmer <lesmana@gmx.de>
2017-04-16 17:47:33 +0200

    button zeig mir die loesung

--- mehraufgaben.js
+++ mehraufgaben.js
@@ -1,12 +1,22 @@
-//'use strict';
+'use strict';
 
 function nocheineaufgabe() {
+  var volumen_elem = document.getElementById("volumen");
+  volumen_elem.innerHTML = "??";
   var radius = rand(1, 10);
   var hoehe = rand(1, 20);
   var radius_elem = document.getElementById("radius");
   var hoehe_elem = document.getElementById("hoehe");
   radius_elem.innerHTML = radius;
   hoehe_elem.innerHTML = hoehe;
+  var volumen = Math.PI * radius * radius * hoehe
+  sessionStorage.setItem("volumen", volumen)
+}
+
+function zeigmirdieloesung() {
+  var volumen = sessionStorage.getItem("volumen");
+  var volumen_elem = document.getElementById("volumen");
+  volumen_elem.innerHTML = volumen;
 }
 
 function rand(min, max) {			
