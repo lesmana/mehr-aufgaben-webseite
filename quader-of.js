3797929dd811cb9b77d4e75befa5aca6f03d09f2
Lesmana Zimmer <lesmana@gmx.de>
2017-04-19 20:56:50 +0200

    add quader oberfläche

---
+++ quader-of.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function nocheineaufgabe() {
+  var loesung_elem = document.getElementById("loesung");
+  loesung_elem.style.visibility = "hidden";
+  var laenge = rand(5, 20);
+  var breite = rand(5, 20);
+  var hoehe = rand(10, 50);
+  var laenge_elem = document.getElementById("laenge");
+  var breite_elem = document.getElementById("breite");
+  var hoehe_elem = document.getElementById("hoehe");
+  laenge_elem.innerHTML = laenge;
+  breite_elem.innerHTML = breite;
+  hoehe_elem.innerHTML = hoehe;
+  var fl1 = laenge * breite
+  var fl2 = breite * hoehe
+  var fl3 = hoehe * laenge
+  var oberflaeche = fl1 + fl1 + fl2 + fl2 + fl3 + fl3
+  oberflaeche = Math.round(oberflaeche * 100) / 100
+  var oberflaeche_elem = document.getElementById("oberflaeche");
+  oberflaeche_elem.innerHTML = oberflaeche;
+}
+
+function zeigmirdieloesung() {
+  var loesung_elem = document.getElementById("loesung");
+  loesung_elem.style.visibility = "visible";
+}
+
+function rand(min, max) {			
+  var randval = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randval;
+} 
+
